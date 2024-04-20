59df9e2ce1e8fe402377010918d752f7f545f3c9
sambhavmahajan <70747596+sambhavmahajan@users.noreply.github.com>
2024-04-20 18:26:23 +0530

    Corrected the logic.

--- SimilarZ/Form1.cs
+++ SimilarZ/Form1.cs
@@ -31,82 +31,49 @@ namespace SimilarZ
             }
         }
 
-        private Dictionary<string, List<int>> MapIndicesToWords(string[] words)
-        {
-            Dictionary<string, List<int>> indexMap = new Dictionary<string, List<int>>();
-            for (int i = 0; i < words.Length; i++)
-            {
-                string word = words[i].ToLower();
-                if (!indexMap.ContainsKey(word))
-                {
-                    indexMap[word] = new List<int>();
-                }
-                indexMap[word].Add(i);
-            }
-            return indexMap;
-        }
-
         public int CalculateConsecutiveSimilarity(string s1, string s2)
         {
+            int res = 0;
             string[] separators = { " ", ",", ".", "?", "!", ";" };
             string[] words1 = s1.Split(separators, StringSplitOptions.RemoveEmptyEntries);
             string[] words2 = s2.Split(separators, StringSplitOptions.RemoveEmptyEntries);
 
-            Dictionary<string, List<int>> wordIndicesMap1 = MapIndicesToWords(words1);
-            Dictionary<string, List<int>> wordIndicesMap2 = MapIndicesToWords(words2);
-
-            int res = 0;
-            int currentConsecutiveSimilarity = 0;
-
-            foreach (var word in wordIndicesMap1.Keys)
+            int count = 0;
+            for (int i = 0; i < words1.Length; i++)
             {
-                if (wordIndicesMap2.ContainsKey(word))
+                int j = 0;
+                while (j < words2.Length)
                 {
-                    List<int> indices1 = wordIndicesMap1[word];
-                    List<int> indices2 = wordIndicesMap2[word];
-
-                    int index1Index = 0;
-                    int index2Index = 0;
-                    while (index1Index < indices1.Count && index2Index < indices2.Count)
+                    if (words1[i] == words2[j])
                     {
-                        int index1 = indices1[index1Index];
-                        int index2 = indices2[index2Index];
-
-                        if (index2 - index1 == index2Index - index1Index)
+                        int tempJ = j;
+                        while (i < words1.Length && tempJ < words2.Length && words1[i] == words2[tempJ])
                         {
-                            currentConsecutiveSimilarity++;
+                            count++;
+                            i++;
+                            tempJ++;
                         }
-                        else
+                        if (count >= 3)
                         {
-                            if (currentConsecutiveSimilarity >= 2)
-                            {
-                                res += currentConsecutiveSimilarity;
-                            }
-                            currentConsecutiveSimilarity = 0;
+                            res += count;
                         }
-
-                        index1Index++;
-                        index2Index++;
+                        count = 0;
                     }
+                    j++;
                 }
-            }
-
-            if (currentConsecutiveSimilarity >= 2)
-            {
-                res += currentConsecutiveSimilarity;
             }
 
             return res;
         }
 
-    private void button3_Click(object sender, EventArgs e)
+        private void button3_Click(object sender, EventArgs e)
         {
             string s1 = richTextBox1.Text.ToLower();
             string s2 = richTextBox2.Text.ToLower();
 
             int res = CalculateConsecutiveSimilarity(s1, s2);
 
-            double similarityPercentage = (double)res / (s1.Split(' ').Length) * 100;
+            double similarityPercentage = ((double)res) / (s1.Split(' ').Length) * 100;
  
             progressBar1.Value = (int)similarityPercentage;
             label1.Text = $"{similarityPercentage:F2}%";
