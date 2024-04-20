b7912694fef2d1482514879d868197f5ef80eb78
sambhavmahajan <70747596+sambhavmahajan@users.noreply.github.com>
2024-04-20 15:28:11 +0530

    Restructure and typo fix.

--- SimilarZ/Form1.Designer.cs
+++ SimilarZ/Form1.Designer.cs
@@ -104,9 +104,9 @@
             label1.AutoSize = true;
             label1.Location = new Point(803, 415);
             label1.Name = "label1";
-            label1.Size = new Size(23, 15);
+            label1.Size = new Size(38, 15);
             label1.TabIndex = 6;
-            label1.Text = "0%";
+            label1.Text = "0.00%";
             // 
             // label2
             // 
--- SimilarZ/Form1.cs
+++ SimilarZ/Form1.cs
@@ -1,7 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
 namespace SimilarZ
 {
     public partial class Form1 : Form
     {
+        private HashSet<string> genericWords = new HashSet<string>
+        {
+            "a", "an", "and", "as", "at", "be", "by", "for", "from", "has", "have",
+            "he", "her", "him", "his", "in", "is", "it", "its", "of", "on", "or",
+            "that", "the", "their", "there", "they", "this", "to", "was", "were",
+            "which", "with", "you"
+        };
+
         public Form1()
         {
             InitializeComponent();
@@ -22,41 +36,25 @@ namespace SimilarZ
                 richTextBox2.Text = File.ReadAllText(openFileDialog1.FileName);
             }
         }
-        private HashSet<string> genericWords = new HashSet<string>
+
+        private int CalculateSimilarity(string s1, string s2)
         {
-            "a", "an", "and", "as", "at", "be", "by", "for", "from", "has", "have",
-            "he", "her", "him", "his", "in", "is", "it", "its", "of", "on", "or",
-            "that", "the", "their", "there", "they", "this", "to", "was", "were",
-            "which", "with", "you"
-        };
-        private int similar(string x, string Y)
-        {
-            int res = 0;
-            string s1 = richTextBox1.Text.ToString();
-            string s2 = richTextBox2.Text.ToString();
-            HashSet<string> words = new HashSet<string>();
             string[] words1 = s1.Split(new char[] { ' ', ',', '.', '?', '!', ';' }, StringSplitOptions.RemoveEmptyEntries);
-            foreach (string s in words1)
-            {
-                words.Add(s);
-            }
-            foreach (string s in words)
-            {
-                if (genericWords.Contains(s) == false)
-                {
-                    if (words.Contains(s))
-                    {
-                        res++;
-                    }
-                }
-            }
-            return res;
+            HashSet<string> words = new HashSet<string>(words1, StringComparer.OrdinalIgnoreCase);
+            words.ExceptWith(genericWords);
+            return words.Count(word => s2.IndexOf(word, StringComparison.OrdinalIgnoreCase) >= 0);
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            int res = similar(richTextBox1.Text.ToString().ToLower(), richTextBox2.Text.ToString().ToLower());
-            progressBar1.Value = res / (richTextBox1.Text.Length + richTextBox2.Text.Length) * 2;
+            string s1 = richTextBox1.Text.ToLower();
+            string s2 = richTextBox2.Text.ToLower();
+
+            int similarity = CalculateSimilarity(s1, s2);
+            double similarityPercentage = (double)similarity / (s1.Length + s2.Length) * 100;
+
+            progressBar1.Value = (int)similarityPercentage;
+            label1.Text = $"{similarityPercentage:F2}%";
         }
     }
 }
